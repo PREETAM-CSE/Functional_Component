94bb09b8831c6bf8004c0d7e5bd0015ed2766fc2
preetam-kn <preetam.kumar@klearnow.com>
2024-12-16 06:22:18 +0530

    Functional Components demo

---
+++ src/Components/Blog.js
@@ -0,0 +1,122 @@
+//Blogging App using Hooks
+import { type } from "@testing-library/user-event/dist/type";
+import { useState, useRef, useEffect, useReducer } from "react";
+
+function blogsReducer(state, action){
+  switch(action.type){
+    case "ADD" : 
+        return [action.blog, ...state];
+    case "REMOVE" :
+        return state.filter((blog,index) => index !== action.index);
+    default : {
+        return [];
+    }        
+  }
+}
+
+export default function Blog(){
+
+    // const [title,setTitle] = useState("");
+    // const [content,setContent] = useState("");
+    const [formData, setformData] = useState({title:"", content:""})
+   // const [blogs, setBlogs] =  useState([]); //We will reducers in place of useState
+   const [blogs, dispatch] = useReducer(blogsReducer, []);
+    const titleRef = useRef(null);
+
+    useEffect(()=>{
+        titleRef.current.focus();
+    }, []);
+
+    useEffect(() => {
+        if(blogs.length && blogs[0].title){
+            document.title = blogs[0].title;
+        }else{
+            document.title = "No Blogs";
+        }
+    }, [blogs])
+
+    function handleSubmit(e){
+        e.preventDefault();
+
+       // setBlogs([{title: formData.title,content:formData.content}, ...blogs]); //Commenting as using reducer so dispach will be used in place
+        dispatch({type: "ADD", blog : {title: formData.title,content:formData.content} })
+        setformData({title:"", content:""});
+        console.log(blogs);
+        titleRef.current.focus();
+    }
+
+    function removeBlog(i){
+
+        //setBlogs( blogs.filter((blog,index)=> index !== i)); // Commenting as we will use reducer so need to use dispatch
+        dispatch({type : "REMOVE", index : i})
+ 
+     }
+
+    return(
+        <>
+        <h1>Write a Blog!</h1>
+        <div className="section">
+
+        {/* Form for to write the blog */}
+            <form onSubmit={handleSubmit}>
+                <Row label="Title"> 
+                        <input className="input"
+                                placeholder="Enter the Title of the Blog here.."
+                                value={formData.title}
+                                ref={titleRef}
+                                onChange = {(e) => setformData({title: e.target.value, content:formData.content})}
+                        />
+                </Row >
+
+                <Row label="Content">
+                        <textarea className="input content"
+                                placeholder="Content of the Blog goes here.."
+                                value={formData.content}
+                                required
+                                onChange = {(e) => setformData({title: formData.title,content: e.target.value})}
+                        />
+                </Row >
+         
+                <button className = "btn">ADD</button>
+            </form>
+                     
+        </div>
+
+        <hr/>
+
+        {/* Section where submitted blogs will be displayed */}
+        <h2> Blogs </h2>
+        {blogs.map((blog,i) => (
+            <div className="blog">
+                <h3>{blog.title}</h3>
+                <hr/>
+                <p>{blog.content}</p>
+
+                <div className="blog-btn">
+                        <button onClick={() => {
+                            removeBlog(i)
+                        }}
+                        className="btn remove">
+
+                            Delete
+
+                        </button>
+                </div>
+            </div>
+        ))}
+        
+        </>
+        )
+    }
+
+//Row component to introduce a new row section in the form
+function Row(props){
+    const{label} = props;
+    return(
+        <>
+        <label>{label}<br/></label>
+        {props.children}
+        <hr />
+        </>
+    )
+}
